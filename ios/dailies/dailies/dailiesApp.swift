1f474d9533ac3f7863f7bfd4f0e0614899c2d7d6
David Wetterau <david.wetterau@gmail.com>
2024-12-01 12:10:49 -0500

    initial ios app commit

---
+++ ios/dailies/dailies/dailiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailiesApp.swift
+//  dailies
+//
+//  Created by David Wetterau on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct dailiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/dailies/dailiesTests/dailiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  dailiesTests.swift
+//  dailiesTests
+//
+//  Created by David Wetterau on 12/1/24.
+//
+
+import Testing
+@testable import dailies
+
+struct dailiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
