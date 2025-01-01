99e07483ec5213523140361ab6f18c9bb9331a4f
David Wetterau <david.wetterau@gmail.com>
2025-01-01 13:54:45 -0500

    simplify init

--- ios/dailies/dailies/Views/ExercisePages/WorkoutDetailsForm.swift
+++ ios/dailies/dailies/Views/ExercisePages/WorkoutDetailsForm.swift
@@ -20,11 +20,11 @@ func getDurationSeconds(forHoursMinutesSeconds input: (hours: Int, minutes: Int,
 }
 
 struct WorkoutDetailsForm: View {
-    let weight: Binding<Double?>
-    let numReps: Binding<Int?>
-    let numSets: Binding<Int?>
-    let durationSeconds: Binding<Double?>
-    let distance: Binding<Double?>
+    @Binding var weight: Double?
+    @Binding var numReps: Int?
+    @Binding var numSets: Int?
+    @Binding var durationSeconds: Double?
+    @Binding var distance: Double?
     let isDisabled: Bool
     let isFieldRequired: (_ fieldName: String) -> Bool
 
@@ -32,37 +32,29 @@ struct WorkoutDetailsForm: View {
     @State private var minutes: Int = 0
     @State private var seconds: Int = 0
     @State private var isInitialized: Bool = false
-    
-    init(weight: Binding<Double?>, numReps: Binding<Int?>, numSets: Binding<Int?>, durationSeconds: Binding<Double?>, distance: Binding<Double?>, isDisabled: Bool, isFieldRequired: @escaping (_: String) -> Bool) {
-        self.weight = weight
-        self.numReps = numReps
-        self.numSets = numSets
-        self.durationSeconds = durationSeconds
-        self.distance = distance
-        self.isDisabled = isDisabled
-        self.isFieldRequired = isFieldRequired
-    }
-    
+
     private func initializeHoursMinutesSeconds() {
-        if (isInitialized || durationSeconds.wrappedValue == nil) {
+        if isInitialized {
             return
         }
-        self.isInitialized = true
-        let (hours, minutes, seconds) = getHoursMinutesSeconds(forDurationSeconds: durationSeconds.wrappedValue!)
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
+        isInitialized = true
+        if let durationSeconds = durationSeconds {
+            let (hours, minutes, seconds) = getHoursMinutesSeconds(forDurationSeconds: durationSeconds)
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+        }
     }
 
     private func updateDurationSeconds() {
-        durationSeconds.wrappedValue = getDurationSeconds(forHoursMinutesSeconds: (hours, minutes, seconds))
+        durationSeconds = getDurationSeconds(forHoursMinutesSeconds: (hours, minutes, seconds))
     }
 
     var body: some View {
         if isFieldRequired("weight") {
             HStack {
                 Text("Weight")
-                TextField("0", value: weight, format: .number)
+                TextField("0", value: $weight, format: .number)
                     .keyboardType(.decimalPad)
                     .disabled(isDisabled)
                     .multilineTextAlignment(.trailing)
@@ -72,7 +64,7 @@ struct WorkoutDetailsForm: View {
         if isFieldRequired("numReps") {
             HStack {
                 Text("Repetitions")
-                TextField("0", value: numReps, format: .number)
+                TextField("0", value: $numReps, format: .number)
                     .keyboardType(.numberPad)
                     .disabled(isDisabled)
                     .multilineTextAlignment(.trailing)
@@ -81,7 +73,7 @@ struct WorkoutDetailsForm: View {
         if isFieldRequired("numSets") {
             HStack {
                 Text("Sets")
-                TextField("0", value: numSets, format: .number)
+                TextField("0", value: $numSets, format: .number)
                     .keyboardType(.numberPad)
                     .disabled(isDisabled)
                     .multilineTextAlignment(.trailing)
@@ -90,7 +82,7 @@ struct WorkoutDetailsForm: View {
         if isFieldRequired("distance") {
             HStack {
                 Text("Distance")
-                TextField("0", value: distance, format: .number)
+                TextField("0", value: $distance, format: .number)
                     .keyboardType(.decimalPad)
                     .disabled(isDisabled)
                     .multilineTextAlignment(.trailing)
@@ -146,7 +138,6 @@ struct WorkoutDetailsForm: View {
                     .frame(minWidth: 30)
                     .foregroundColor(.primary)
             }.disabled(isDisabled)
-
             Text("sec")
         }
         .onChange(of: hours) { updateDurationSeconds() }
