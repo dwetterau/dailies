8702cfaa08835f059eb1a87c85e1edd344bfb514
David Wetterau <david.wetterau@gmail.com>
2025-03-31 20:46:05 -0400

    auth and fetching is fully working

--- dailies-react-native/app/_layout.tsx
+++ dailies-react-native/app/_layout.tsx
@@ -1,36 +1,55 @@
 import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
-import {useAuth0, Auth0Provider} from 'react-native-auth0';
-import Constants from 'expo-constants';
+import { useAuth0, Auth0Provider } from "react-native-auth0";
+import Constants from "expo-constants";
 
 import { Stack } from "expo-router";
 import { useCallback, useMemo } from "react";
 
-const {EXPO_PUBLIC_CONVEX_URL} = Constants.expoConfig?.extra ?? {};
+const { EXPO_PUBLIC_CONVEX_URL } = Constants.expoConfig?.extra ?? {};
 
 const convex = new ConvexReactClient(EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
+  verbose: true,
 });
 
 function useAuthFromAuth0() {
-  const {isLoading, user, getCredentials} = useAuth0();
+  const { isLoading, user, getCredentials } = useAuth0();
+  const isAuthenticated = !!user;
+
   const fetchAccessToken = useCallback(
-    async ({forceRefreshToken}: {forceRefreshToken: boolean}) => {
+    async ({ forceRefreshToken }: { forceRefreshToken: boolean }) => {
       try {
-        const response = await getCredentials();
-        return response?.accessToken ?? null;
+        const response = await getCredentials(
+          "openid email profile offline_access",
+          undefined,
+          {},
+          forceRefreshToken
+        );
+        const idToken = response?.idToken ?? null;
+        return idToken;
       } catch (e) {
-        console.log("unable to get auth0 credentials", e);
+        console.error("Error fetching token", e);
         return null;
       }
-    }, [getCredentials]);
+    },
+    [getCredentials]
+  );
 
-    return useMemo(() => ({isLoading, isAuthenticated: !!user, fetchAccessToken}), [isLoading, user, fetchAccessToken]);
+  return useMemo(
+    () => ({ isLoading, isAuthenticated, fetchAccessToken }),
+    [isLoading, isAuthenticated, fetchAccessToken]
+  );
 }
 
 export default function RootLayout() {
-  return <Auth0Provider domain={"dev-0a4aznywud2xhrvr.us.auth0.com"} clientId={"ltcnlDIWJ6GxDCWpdW1fsosaPSR4KwCZ"}>
+  return (
+    <Auth0Provider
+      domain={"dev-0a4aznywud2xhrvr.us.auth0.com"}
+      clientId={"ltcnlDIWJ6GxDCWpdW1fsosaPSR4KwCZ"}
+    >
       <ConvexProviderWithAuth client={convex} useAuth={useAuthFromAuth0}>
         <Stack />
       </ConvexProviderWithAuth>
-    </Auth0Provider>;
+    </Auth0Provider>
+  );
 }
--- dailies-react-native/app/index.tsx
+++ dailies-react-native/app/index.tsx
@@ -1,27 +1,32 @@
 import { useQuery } from "convex/react";
-import {api} from "@convex/_generated/api"
+import { api } from "@convex/_generated/api";
 import { Button, Text, View } from "react-native";
 import { useAuth0 } from "react-native-auth0";
 
 export default function Index() {
-  const {user, authorize} = useAuth0();
+  const { user, authorize } = useAuth0();
 
   if (user) {
-    return <EntityList />
+    return <EntityList />;
   }
-  return <View>
-    <Button onPress={async () => {
-      try {
-        await authorize();
-      } catch (e) {
-        console.log('Error logging in', e);
-      }
-    }} title="Log in" />
-  </View>
+  return (
+    <View>
+      <Button
+        onPress={async () => {
+          try {
+            await authorize({ scope: "openid email profile offline_access" });
+          } catch (e) {
+            console.log("Error logging in", e);
+          }
+        }}
+        title="Log in"
+      />
+    </View>
+  );
 }
 
 function EntityList() {
-  const entities = useQuery(api.entities.list, {})
+  const entities = useQuery(api.entities.list, {});
   return (
     <View
       style={{
@@ -30,7 +35,9 @@ function EntityList() {
         alignItems: "center",
       }}
     >
-      {entities?.entities.map(entity => <Text key={entity._id}>{entity.name}</Text>)}
+      {entities?.entities.map((entity) => (
+        <Text key={entity._id}>{entity.name}</Text>
+      ))}
     </View>
   );
 }
